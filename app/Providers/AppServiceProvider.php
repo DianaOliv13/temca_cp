38927988ca8c98058f241db04db857e8a62b0db3
DianaOliv13 <2523160149@e.uttecamac.edu.mx>
2026-06-29 13:39:10 -0600

    Forzar HTTPS en producción

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,6 +14,10 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
+
         Route::resourceVerbs([
             'create' => 'crear',
             'edit'   => 'editar',
